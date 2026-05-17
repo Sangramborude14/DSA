0b4f580331d8e78ae7b5799ecf4fd0e5583fdab1
Sangram <sangram2mail@gmail.com>
2026-05-17 19:37:42 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   float x = 44;
+   cout << "the value of x is " << x;
+   return 0;
+}
---
+++ userInput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "enter your name\n";
+    string name;
+    cin >> name;
+    cout << "your name is "<< name;    
+    return 0;
+}
