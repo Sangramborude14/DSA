eda740f1304029c3ec44d63ec623a5831311f2d1
Sangram <sangram2mail@gmail.com>
2026-05-19 22:17:18 +0530

    last commit for today

--- pattern.cpp
+++ pattern.cpp
@@ -37,6 +37,49 @@ void pattern4(int n){
     }
 }
 
+void pattern5(int n){
+    for(int i=0; i <= n ; i++){
+        for(int j = n-i; j > 0 ; j--){
+            cout << "*" << " ";
+        };
+        cout << "\n";
+    };
+}
+
+void pattern6(int n){
+    for(int i=0; i < n; i++){
+      //space
+      for(int j=0; j < n-i-1;j++){
+        cout << " ";
+      };
+      for(int j=0; j < 2*i + 1;j++){
+        cout << "*";
+      };
+      for(int j=0; j < n-i-1;j++){
+        cout << " ";
+      }
+      cout << "\n";
+    };
+}
+
+void pattern7(int n){
+    for(int i=0; i<n ; i++){
+
+        for(int j=0; j<i ; j++){
+            cout << " ";
+        };
+        
+        for(int j=0; j< 2*(n-i-1)+1 ; j++ ){
+            cout << "*";
+        };
+        
+        for(int j=0; j<i ; j++){
+            cout << " ";
+        };
+        cout << "\n";
+        
+    }
+}
 int main() {
     // for the outer loop count the number of rows
     // for the inner loop focus on the coloumn and connect them to the rows
@@ -47,6 +90,8 @@ int main() {
     pattern2(5);
     pattern3(5);
     pattern4(5);
-
+    pattern5(5);
+    pattern6(5);
+    pattern7(5);
     return 0;
 }
