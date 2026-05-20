c6b3493221816383d4575115414de2811ada7ba1
Sangram <sangram2mail@gmail.com>
2026-05-20 22:51:27 +0530

    final commit

--- pattern.cpp
+++ pattern.cpp
@@ -80,6 +80,34 @@ void pattern7(int n){
         
     }
 }
+
+void pattern8(int n){
+    for(int i=1 ;i <= 2*n-1; i++){
+        int stars = i;
+        if(i > n) stars = 2*n-i ;
+        for(int j=1; j<= stars; j++){
+
+        }
+    } ;
+}
+
+void pattern9(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            // If i and j are both even or both odd, prints 1, else 0
+            cout << ((i + j) % 2 == 0 ? "1 " : "0 ");
+        }
+        cout << "\n";
+    }
+}
+
+void pattern10(int n){
+    for(int i=0; i < n; i++){
+        for(int j=0; j < 2*n-1;j++){
+            ((j == 0)||(2*n-2)) ? cout << "1" : cout << " ";
+        };
+    }
+}
 int main() {
     // for the outer loop count the number of rows
     // for the inner loop focus on the coloumn and connect them to the rows
@@ -93,5 +121,8 @@ int main() {
     pattern5(5);
     pattern6(5);
     pattern7(5);
+    pattern8(5);
+    pattern9(5);
+    pattern10(4);
     return 0;
 }
