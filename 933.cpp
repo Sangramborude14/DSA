b416a9f6407b3ee077d60509b26e3dd2e31940f5
Sangram <sangram2mail@gmail.com>
2026-06-11 21:31:12 +0530

    LeetCode: #933 Number of recent calls

---
+++ 933.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class RecentCounter(){
+while(q.front() < t - 3000){
+    q.pop();
+}
+return q.size();
+}
+
+int ping(int t){
+q.push(t)
+}
+int main() {
+    
+    return 0;
+}
