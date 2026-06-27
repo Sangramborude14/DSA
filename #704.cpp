bcdff17ec732f45553b084e29dd9b0b7f0953027
Sangram Borude <sangram2mail@gmail.com>
2026-06-27 09:42:40 +0530

    Leetcode: #704 Binary Search

---
+++ #704.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+ int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(right > left){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] = target) return mid;
+        if(nums[mid] > target) right = mid - 1;
+        if(nums[mid] < target) left = mid + 1;
+
+        }
+        return -1;
+    }
+
+int main() {
+    
+    return 0;
+}
