68ceeb369e73a5812b03ce141ad698f99d58a333
Sangram Borude <sangram2mail@gmail.com>
2026-07-04 07:14:57 +0530

    LeetCode: #1 Two Sum

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+  unordered_map<int, int> hashmap;
+ for(int i=0;i < nums.size(); i++){
+    if(hashmap.count(target-nums[i])){
+        return {i,hashmap[target-nums[i]]};
+    }
+    hashmap[nums[i]] = i;
+ };
+ return {};
+    };
+
+int main() {
+    
+    return 0;
+}
