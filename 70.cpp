a46cffddc290a8f79c2e604a794e80e09960b2ef
Sangram Borude <sangram2mail@gmail.com>
2026-07-09 22:57:56 +0530

    LeetCode:  #70 Climbing Stairs

---
+++ 70.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int climbStairs(int n) {
+        if (n <= 2) return n;
+        //fibonacci
+        int prev2 = 1;
+        int prev1 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+int main() {
+    
+    return 0;
+}
