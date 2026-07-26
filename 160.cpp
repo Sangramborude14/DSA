b398b44ec0fae5570859919cd1673689dcd2ed78
Sangram <sangram2mail@gmail.com>
2026-07-26 23:17:11 +0530

    LeetCode: #160 contains nearby duplicate

---
+++ 160.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        return nullptr;
+    }
+};
+
+int main() {
+    Solution solver;
+    cout << "Implement and test your getIntersectionNode solution here." << endl;
+    return 0;
+}
