63cb33cf85032eaab9f7efe90d90ea28fddfe332
Sangram <sangram2mail@gmail.com>
2026-07-31 21:36:40 +0530

    LeetCode:  #159 longest substring containing at most two distinct characters

---
+++ 159.cpp
@@ -0,0 +1,20 @@
+int longestSubstringContainingAtMostTwoDistinctCharacters(string s){
+    int left = 0;
+    unordered_map<char,int> freq;
+    int ans = 0;
+
+    for(int right = 0; right < s.size(); right++){
+        freq[s[right]]++;
+
+        while(freq.size() > 2){
+            freq[s[left]]--;
+
+            if(freq[s[left]] == 0){
+                freq.erase(s[left]);
+            }
+            left++;
+        }
+        ans = max(ans,right - left + 1);
+    }
+    return ans;
+}
--- slidingWindow.cpp
+++ slidingWindow.cpp
@@ -1,26 +1,119 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
-int maxSubarraySum(vector<int>& nums, int k){
-int current_sum = 0;
-int max_sum = 0;
-
-for(int i = 0; i < k; i++){
-    current_sum += nums[i];
-}
-for(int i = k; i < nums.size(); i++){
-    current_sum += nums[i];
-
-    if(max_sum < current_sum){
-    max_sum = current_sum;
-}
-}
-
-return max_sum;
-}
-
 using namespace std;
+
+// Fixed Window Template
+int left = 0;
+int sum = 0;
+
+for(int right=0;right < n; right++){
+    sum += arr[right];
+
+    if(right - left + 1 == k){ //current window size || window expands till window size is reached
+        
+        sum -= arr[left]; // remove the left element
+        left++; // to maintain the window size
+    }
+}
+
+// Find maximum sum of a contiguous subarrray of size k
+int subArraySum(vector<int>& nums,int k){
+    int left = 0;
+    int sum = 0;
+    int maxSum = 0;
+
+    for(int right = 0; right < nums.size();right++){
+        sum += nums[right];
+        
+        if(right - left + 1 == k ){
+            if(sum > maxSum) maxSum = sum;
+            sum -= nums[left];
+            left++;
+        }
+    }
+    return maxSum;
+}
+
+
+// Find minimum length subarray whose sum is at least 7
+int minLengthSubArray(vector<int>& nums,int k){
+    int left = 0;
+    int sum = 0;
+    int ans =INT_MAX;
+
+    for(int right = 0; right < nums.size(); right++){
+        sum += nums[right];
+        while(sum >= k){
+            ans = min(ans,right - left + 1);
+            sum -= nums[left];
+            left++;
+        }
+    }
+return ans;
+}
+
+
+// maximum window problem
+int maxWindow(vector<int>& nums,int target){
+     int left = 0;
+     int sum = 0;
+     int ans= 0;
+     
+     for(int right=0;right < nums.size(); right++){
+        sum += nums[right];
+        while(sum > target){
+            ans = max(ans,right - left + 1);
+            sum -= left;
+            left++;
+
+        }
+     }
+     return ans;
+}
+
+
+// Frequency based sliding window
+
+// Longest substring without repeating character
+int norepeatingChar(string s){
+    unordered_set<char> seen;
+    int left = 0;
+    int ans = 0;
+
+    for(int right=0; right < s.size(); right++){
+        if(seen.find(s[right]) == seen.end()){
+            seen.insert(s[right]);
+            ans = max(ans,right - left + 1);
+        }
+        while(seen.find(s[right]) != seen.end()){
+            seen.erase(s[left]);
+            left++;
+        }
+    }
+    return ans;
+}
+
+//longest substring containing at most 2 distinct characters
+int longestSubstringContainingAtMostTwoDistinctCharacters(string s){
+    int left = 0;
+    unordered_map<char,int> freq;
+    int ans = 0;
+
+    for(int right = 0; right < s.size(); right++){
+        freq[s[right]]++;
+
+        while(freq.size() > 2){
+            freq[s[left]]--;
+
+            if(freq[s[left]] == 0){
+                freq.erase(s[left]);
+            }
+            left++;
+        }
+        ans = max(ans,right - left + 1);
+    }
+    return ans;
+}
 
 int main() {
     
