3bd74254f49a72defa76eb2d798425ccdd948a35
Sangram <sangram2mail@gmail.com>
2026-08-08 16:42:09 +0530

    LeetCode: #153 Find Mininum element in a rotated array

---
+++ 153.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+ int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans;
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] > nums[high]){ // Left side is sorted
+             low = mid + 1;
+            }else{
+                high = mid;
+            }
+            ans = mid;
+        }
+        return nums[ans];
+    }
+
+int main() {
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int result = findMin(nums);
+    cout << result << endl;
+    return 0;
+}
