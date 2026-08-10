6c6c5c32718b88c5aed75b08a10c5d0edf8beed2
Sangram <sangram2mail@gmail.com>
2026-08-10 21:30:38 +0530

    LeetCode: #367 is Perfect Square

---
+++ 367.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+ bool isPerfectSquare(int num) {
+    long long low = 0;
+    int high = num;
+    
+   while(low <= high){
+    long long  mid = low + (high - low)/2;
+    if(mid * mid == num){
+        return true;
+    }
+    else if(mid*mid < num){
+        low = mid + 1;
+    }else{
+        high = mid - 1;
+    }
+   }
+   return false;
+    }
+
+int main() {
+    cout << isPerfectSquare(16);
+    return 0;
+}
