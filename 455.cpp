7cea176f77b11590b2ec6db80793bb4192fadcf1
Sangram <sangram2mail@gmail.com>
2026-08-12 21:23:47 +0530

    LeetCode: #455 Greed and Cookie

---
+++ 455.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+  int findContentChildren(vector<int>& g, vector<int>& s) {
+     sort(g.begin(),g.end());
+     sort(s.begin(),s.end());
+     
+     int child = 0;
+     int cookie = 0;
+     
+     while(child < g.size() && cookie < s.size()){
+
+        if(s[cookie] >= g[child]){
+            child++;
+        };
+        cookie++;
+     }
+     return child;
+    }
+
+int main() {
+    
+    return 0;
+}
