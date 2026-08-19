d522df8faeae3ca9d214296f9efcd5c521a3f00e
Sangram <sangram2mail@gmail.com>
2026-08-19 22:05:47 +0530

    LeetCode: #28 Find the index of first occurance of substring

---
+++ 28.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int strStr(string haystack, string needle) {
+
+        return haystack.find(needle);
+    }
+
+int main() {
+    
+    return 0;
+}
