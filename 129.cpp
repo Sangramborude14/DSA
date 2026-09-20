1fd481941ac9654664d8b104f5f029b40f0fd580
Sangram <sangram2mail@gmail.com>
2026-09-20 21:16:54 +0530

    LeetCode : @129 Sum root to Leaf Number

---
+++ 129.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int currentSum) {
+        if (!node) return 0;
+        
+        currentSum = currentSum * 10 + node->val;
+        
+        if (!node->left && !node->right) {
+            return currentSum;
+        }
+        
+        return dfs(node->left, currentSum) + dfs(node->right, currentSum);
+    }
+
+int main() {
+    
+    return 0;
+}
