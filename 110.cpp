1058d0dbf890a58a6b1e81f157ed1b7b38a0a0f4
Sangram <sangram2mail@gmail.com>
2026-09-23 00:55:47 +0530

    LeetCode 110 correction

--- 110.cpp
+++ 110.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int height(TreeNode* root){
     if(root == nullptr) return 0;
 
-    int leftHeight = checkHeight(root->left);
+    int leftHeight = height(root->left);
     if(leftHeight == -1) return -1;
 
-    int rightHeight = checkHeight(root->right);
+    int rightHeight = height(root->right);
     if(rightHeight == -1) return -1;
 
     if(abs(leftHeight - rightHeight) > 1) return -1;
@@ -17,7 +17,7 @@ int height(TreeNode* root){
 }
 
 bool isBalanced(TreeNode* root) {
-        return height(root) == -1;
+        return height(root) != -1;
     }
 
 int main() {
